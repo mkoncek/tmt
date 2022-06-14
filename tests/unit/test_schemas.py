18e2194d19e06a0c699f3012c3f02dd186a2e87a
Miloš Prchlík <mprchlik@redhat.com>
2022-06-14 21:46:52 +0200

    Adds helper functions for loading JSON schemas

--- tests/unit/test_schemas.py
+++ tests/unit/test_schemas.py
@@ -5,6 +5,7 @@ import pytest
 from ruamel.yaml import YAML
 
 import tmt
+import tmt.utils
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 SCHEMADIR = os.path.join(PATH, "../../tmt/schemas")
@@ -16,23 +17,7 @@ tmt.base.Tree.init(ROOTDIR, 'empty', False)
 
 @pytest.fixture
 def schema_and_store():
-    # TODO: tmt package shall provide a helper function
-    # for "load schemas for test/plan/story"
-    schema_file = os.path.join(SCHEMADIR, 'tests.yaml')
-    schema = YAML(typ="safe").load(open(schema_file, encoding="utf-8"))
-
-    schema_store = {}
-
-    for schema_name in ('common', 'core'):
-        schema_file = os.path.join(SCHEMADIR, f'{schema_name}.yaml')
-        store_schema = YAML(
-            typ="safe").load(
-            open(
-                schema_file,
-                encoding="utf-8"))
-        schema_store[store_schema['$id']] = store_schema
-
-    return schema, schema_store
+    return tmt.utils.load_schema('tests.yaml'), tmt.utils.load_schema_store()
 
 
 @pytest.fixture(params=tmt.Tree('.').tests())
